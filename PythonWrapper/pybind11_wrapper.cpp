06d500326f4f1af943ad6a59ddbde0eb75ddccc1
CeGa-byte <cedric.gaberle@web.de>
2023-12-07 09:29:46 +0100

    Variable update

--- PythonWrapper/pybind11_wrapper.cpp
+++ PythonWrapper/pybind11_wrapper.cpp
@@ -7,17 +7,16 @@ namespace py = pybind11;
 PYBIND11_MODULE(openqasmparser, m) {
   m.doc() = "Python binding for the OpenQASM parser library.";
   m.def("parse_circuit", &qasmparser::parseCircuit, "Parse an ansatz into the corresponding OpenQASM representation, "
-                                                   "parallel execution enabled.\n"
-                                                   "@param input_fn: Path to the input file to parse.\n"
-                                                   "@param version: OpenQASM version to use, default 2.\n"
-                                                   "@param use_omp: Use OpenMP parallelism, default execution policy "
-                                                   "parallelism.\n"
-                                                   "@param parameterize: Set true to use indication for grouping commuting "
-                                                   "operators.\n"
-                                                   "@param output_fn: Path to (non-)existing file to store the OpenQASM"
-                                                   "representation file. (optional)\n"
-                                                   "@param multiplier: Floating value to be multiplied to each "
-                                                   "operator. (optional)",
+                                                    "parallel execution enabled.\n"
+                                                    "@param input_fn: Path to the input file to parse.\n"
+                                                    "@param version: OpenQASM version to use, default 3.\n"
+                                                    "@param use_omp: Use OpenMP parallelism, default execution policy "
+                                                    "parallelism.\n"
+                                                    "@param parameterize: Set true to parameterize the circuit.\n"
+                                                    "@param output_fn: Path to (non-)existing file to store the OpenQASM"
+                                                    "representation file. (optional)\n"
+                                                    "@param multiplier: Floating value to be multiplied to each "
+                                                    "operator. (optional)",
         py::arg("input_fn"),  // Input file name
         py::kw_only(),
         py::arg("version") = 3,  // OpenQASM version (default v3); Parameterization requires v3!
